583f09d3b080cee8bb2a0f0a8644a9300a7302fd
james-hunter <25466065+james-hunter@users.noreply.github.com>
2026-07-26 10:09:38 +0200

    Update james_get_random.ps1

--- james_get_random.ps1
+++ james_get_random.ps1
@@ -7,35 +7,8 @@ Function James-Get-Random{
     add-type  -AssemblyName System.Windows.Forms
     $max = $count
     $random1 = @(for($i = 0; $i -lt $max; $i++){
-        $pre = [System.Windows.Forms.Cursor]::get_Position()
-        Write-Progress -Activity "Getting random numbers" -PercentComplete (($i / $max)*100)
-        $wait = (Get-Random -Minimum 1 -Maximum 1000)
-        Start-Sleep -Milliseconds $wait   
-        $post = [System.Windows.Forms.Cursor]::get_Position()
-        if($pre -eq $post){
-            Write-Host "$(get-date -format "yyyy-MM-dd HH:mm:ss")`tPlease move the mouse to generate random numbers"
-            do{
-                Start-Sleep -Milliseconds 100
-                $post = [System.Windows.Forms.Cursor]::get_Position()
-            }until($pre -ne $post)
-        }
-        if($post.x -gt $post.y){
-
-            if($post.x -eq 0 ){
-                0
-            }
-            else{
-                $post.y / $post.x
-            }
-        }
-        else{
-            if($post.y -eq 0 ){
-                0
-            }
-            else{
-                $post.x / $post.y
-            }
-        }
+        1/(Get-Random -Minimum 0.0 -Maximum 1.0)
     })
     return $random1
 }
+
